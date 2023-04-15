d1b719b33676c5e8df8bdc94c4f873a1f797888e
Thai Duong Tran <duongtt@cystack.net>
2023-04-15 10:51:07 +0700

    add: add test export data

--- gen_res.py
+++ gen_res.py
@@ -1,7 +1,12 @@
 import os
 from typing import List
 
-RESULT_ROOT_PATH = ""
+RESULT_ROOT_PATH = "results/PRIVATE_TEST"
+RESULT_ROOT_PATH_TEST = "results/test_export"
+
+is_test = True
+if is_test:
+    RESULT_ROOT_PATH = RESULT_ROOT_PATH_TEST
 
 
 class FrameData:
@@ -19,10 +24,13 @@ class SceneData:
     def add_frame_data(self, frame_data: FrameData):
         self.frames.append(frame_data)
 
+    def add_frames_data(self, frames_data: List[FrameData]):
+        self.frames.extend(frames_data)
+
     def gen_scene_path(self) -> str:
         scene_path = os.path.join(RESULT_ROOT_PATH, self.scene_name)
         if not os.path.exists(scene_path):
-            os.mkdir(scene_path)
+            os.makedirs(os.path.abspath(scene_path))
         return scene_path
 
 
@@ -36,7 +44,7 @@ class CamData:
 
     def gen_cam_by_scene_path(self, scene_data: SceneData) -> str:
         scene_path = scene_data.gen_scene_path()
-        cam_path = os.path.join(scene_path, self.cam_name)
+        cam_path = os.path.join(scene_path, self.cam_name + ".txt")
         if not os.path.exists(cam_path):
             with open(cam_path, 'w') as file:
                 pass
@@ -45,7 +53,7 @@ class CamData:
     def construct_cam_data_by_scene(self, scene_data: SceneData) -> str:
         result = ""
         for frame in scene_data.frames:
-            result += f"{frame.frame_name}, ({', '.join(frame.coordinates)}), {frame.time_process}"
+            result += f"{frame.frame_name}, {frame.coordinates}, {frame.time_process}"
 
             # add new line
             result += "\n"
@@ -59,3 +67,55 @@ class CamData:
                 file.write(cam_data)
 
 
+# used for testing
+if __name__ == '__main__':
+    cam1 = CamData("CAM_1")
+    scene1_cam1 = SceneData("SCENE_1")
+
+    cam1.add_scene_data(scene1_cam1)
+    scene1_cam1.add_frames_data([
+        FrameData(
+            "frame_1.jpg",
+            (0,0,1,2),
+            0.3
+        ),
+        FrameData(
+            "frame_2.jpg",
+            (0, 0, 1, 3),
+            1
+        )
+    ])
+    cam1.export_cam_data()
+
+    cam2 = CamData("CAM_2")
+    scene1_cam2 = SceneData("SCENE_1")
+
+    cam2.add_scene_data(scene1_cam2)
+    scene1_cam2.add_frames_data([
+        FrameData(
+            "frame_1.jpg",
+            (0, 0, 1, 2),
+            6
+        ),
+        FrameData(
+            "frame_2.jpg",
+            (0, 0, 1, 3),
+            5
+        )
+    ])
+    scene2_cam2 = SceneData("SCENE_2")
+
+    cam2.add_scene_data(scene2_cam2)
+    scene2_cam2.add_frames_data([
+        FrameData(
+            "frame_1.jpg",
+            (0, 0, 1, 2),
+            100
+        ),
+        FrameData(
+            "frame_2.jpg",
+            (0, 0, 1, 3),
+            5
+        )
+    ])
+    cam2.export_cam_data()
